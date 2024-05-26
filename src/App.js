c54b3dfd17823f180a4cf225b2140b188ecc9328
elyaya26 <ermatova-elina@mail.ru>
2024-05-26 11:49:17 +0600

    мой первый проект by элина

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {useState} from 'react';
+import './App.css'
+
+const App = () => {
+
+    const [fox, setFox] = useState('')
+
+    async function getDucknpx create-react-app <my-second-project>s () {
+        const response = await fetch('https://randomfox.ca/floof/')
+        const data = await response.json()
+        setFox(data.image)
+    }
+
+
+    return (
+        <div className='container'>
+            <input type="button" value='Получить лисичек' onClick={getDucks}/>
+            <img src={fox && fox} alt=""/>
+        </div>
+    );
+};
+
+export default App;
+
+import React from 'react';
+
+class App extends React.Component {
+    fetchData = async () => {
+        try {
+            const response = await fetch('https://randomfox.ca/floof/');
+            const data = await response.json();
+            console.log(data.image);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.fetchData}>Запросить картинку лисы</button>
+            </div>
+        );
+    }
 }
 
 export default App;
+
+
+
+
